a4bbcd1b86222638d4fac49ca6b371a8166e4a2b
ALFRED-VJ-DEV <alfred.vj.dev@gmail.com>
2024-09-16 10:22:01 -0600

    clase_16_sept_2024

--- Teoria/Promesas.js
+++ Teoria/Promesas.js
@@ -1,16 +1,20 @@
-const apiUrl = "https://dragonball-api.com/api/characters?page1&limit=100";
-
+const apiUrl = "https://dragonball-api.com/api/characters?page=2&limit=10";
+const apiRick= "https://rickandmortyapi.com/api/character"
 function obtenerDatos(url){
     fetch(url)
     .then(res => {
-        return res.json();
+        return new Promise((result, reject) => {
+            result(result(res.json()));
+        })
+    }) 
+    .then(res => {
+        console.log({res: res.info.next})
     })
-    
+    .catch(e => {
+       console.log(e);
+        // e.then(res => console.log(res));
+    });
 }
 
-function manejarRespuesta(promise){
-   promise.then(res => console.log({data: res.items}))
-}
 
-console.log(obtenerDatos(apiUrl))
-
+obtenerDatos(apiRick);
