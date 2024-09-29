abbd9b6e90ca5c3c801008b173cad905ac1e9c44
ALFRED-VJ-DEV <alfred.vj.dev@gmail.com>
2024-09-29 11:27:07 -0600

    Add cards

--- Ejercicio/planetas.js
+++ Ejercicio/planetas.js
@@ -1,16 +1,20 @@
 //URL para planetas
 const apiUrl = "https://dragonball-api.com/api/planets";
 
-function obtenerPlanets (Url) {
-    fetch(apiUrl)
-    .then(res => {
-        return new Promise((result, reject) => {
-            result(result(res.json()));
+function obtenerPlanets(Url) {
+    fetch(Url)
+        .then(res => {
+            return new Promise((result, reject) => {
+                if (res) result(result(res.json()));
+                else reject("Sin Información")
+            })
         })
-    })  
-    .then(res => {
-        console.log({res: res.items})
-    })
+        .then(res => {
+            console.log("Entrando a obtener planetas")
+            console.log(JSON.stringify(res, null, 2))
+            console.log({ res: res.items })
+        })
+        .catch(error => console.error(error));
 }
 
-obtenerPlanets();
+obtenerPlanets(apiUrl);
--- Ejercicio/script.js
+++ Ejercicio/script.js
@@ -1,15 +1,51 @@
-const apiUrl = "https://dragonball-api.com/api/characters?page1&limit=100";
+const apiUrl = "https://dragonball-api.com/api/characters?page1&limit=16";
 
-function obtenerData (apiUrl) {
+function obtenerData(apiUrl) {
     fetch(apiUrl)
-    .then(res => {
-        return new Promise((result, reject) => {
-            result(result(res.json()));
+        .then(res => {
+            return new Promise((result, reject) => {
+                result(result(res.json()));
+            })
         })
-    })  
-    .then(res => {
-        console.log({res: res.items})
-    })
+        .then(res => {
+            console.log(JSON.stringify(res, null, 2))
+            console.log({ res: res.items })
+            if (res.items.length) {
+                const ContainerCards = document.getElementById("character-list");
+                res.items.map(e => {
+                    const card = document.createElement("div");
+                    card.className = "card";
+                    const contentCard = document.createElement("div");
+                    contentCard.className = "content-card";
+
+                    // Crear el título <h3> con la clase 'title-character' y el texto 'Goku'
+                    const title = document.createElement('h3');
+                    title.className = 'title-character';
+                    title.textContent = e.name;
+
+                    // Crear la imagen <img> con la clase 'img' y sus atributos src y alt
+                    const img = document.createElement('img');
+                    img.className = 'img';
+                    img.src = e.image;
+                    img.alt = e.name;
+
+                    // Crear el botón <button> con la clase 'btn-more-info-character' y el texto 'Más información'
+                    const button = document.createElement('button');
+                    button.className = 'btn-more-info-character';
+                    button.textContent = 'Más información';
+
+                    // Agregar los elementos al div 'content-card'
+                    contentCard.appendChild(title);
+                    contentCard.appendChild(img);
+                    contentCard.appendChild(button);
+
+                    // Agregar el 'content-card' al div 'card'
+                    card.appendChild(contentCard);
+                    ContainerCards.appendChild(card);
+                });
+            }
+        })
+        .catch(error => console.error(error));
 }
 
 obtenerData(apiUrl);
