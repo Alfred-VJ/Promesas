ecc5a6ebbc1d3c32c2555c9acb49a7db35a8eeb5
ledolasdev <noctis_1990@hotmail.com>
2024-09-29 09:05:55 -0600

    task-resolve

--- Ejercicio/planetas.js
+++ Ejercicio/planetas.js
@@ -1,2 +1,16 @@
 //URL para planetas
 const apiUrl = "https://dragonball-api.com/api/planets";
+
+function obtenerPlanets (Url) {
+    fetch(apiUrl)
+    .then(res => {
+        return new Promise((result, reject) => {
+            result(result(res.json()));
+        })
+    })  
+    .then(res => {
+        console.log({res: res.items})
+    })
+}
+
+obtenerPlanets();
--- Ejercicio/script.js
+++ Ejercicio/script.js
@@ -1,2 +1,15 @@
 const apiUrl = "https://dragonball-api.com/api/characters?page1&limit=100";
 
+function obtenerData (Url) {
+    fetch(apiUrl)
+    .then(res => {
+        return new Promise((result, reject) => {
+            result(result(res.json()));
+        })
+    })  
+    .then(res => {
+        console.log({res: res.items})
+    })
+}
+
+obtenerData();
